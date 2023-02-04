2d3de547f11fd99b915dc884e1d2447647459269
KatarzynaBurdzy <katarzyna.anna.burdzy@gmail.com>
2023-02-04 17:33:43 +0100

    Migration done

--- migrations/20230203235010_create-bin-table.js
+++ migrations/20230203235010_create-bin-table.js
@@ -4,22 +4,22 @@
  */
 exports.up = function (knex) {
   return knex.schema
-    .createTable("Bin", (table) => {
+    .createTable("bin", (table) => {
       table.increments();
       table.text("color").notNullable();
       table.text("name").notNullable();
       table.string("description");
       //   table.text("trashes");
     })
-    .createTable("Trash", (table) => {
+    .createTable("trash", (table) => {
       table.increments();
       table.text("name").notNullable();
       table.string("description");
 
       table.integer("bin_id").unsigned();
-      table.foreign("bin_id").references("id").inTable("Bin");
+      table.foreign("bin_id").references("id").inTable("bin");
     })
-    .createTable("Region", (table) => {
+    .createTable("region", (table) => {
       table.increments();
       table.text("name");
 
@@ -27,17 +27,17 @@ exports.up = function (knex) {
       table
         .foreign("planned_dates_id")
         .references("id")
-        .inTable("Planned dates");
+        .inTable("planned dates");
     })
-    .createTable("Planned dates", (table) => {
+    .createTable("planned dates", (table) => {
       table.increments();
       table.date("date");
 
       table.integer("region_id");
-      table.foreign("region_id").references("id").inTable("Region");
+      table.foreign("region_id").references("id").inTable("region");
 
       table.text("bin_id");
-      table.foreign("bin_id").references("id").inTable("Bin");
+      table.foreign("bin_id").references("id").inTable("bin");
     });
 };
 
@@ -46,12 +46,9 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-  // ? I don't understand what that function is supposed to do
   return knex.schema
     .dropTableIfExists("Planed Dates")
     .dropTableIfExists("Regions")
     .dropTableIfExists("Trash")
     .dropTableIfExists("Bin");
 };
-// run migration: npx knex migrate:latest
-// sqlite studio to see database
